45a56ecb895aaf41c9e1138d673303505bd93e28
Andrew Mihalevich <apmihal@gmail.com>
2017-03-22 17:01:22 -0500

    Added more films to choose from, and Tomorrow's Movie feature

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movieList = ["Fargo", "The Big Lebowski", "Star Wars: A New Hope", "UHF", "2001: A Space Oddyssey"]
 
         # TODO: randomly choose one of the movies, and return it
+        return movieList[random.randrange(0, len(movieList))]
 
-        return "The Big Lebowski"
+        #return "The Big Lebowski"
 
     def get(self):
         # choose a movie by invoking our new function
@@ -20,6 +23,12 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        secondMovie = self.getRandomMovie()
+        while secondMovie == movie:
+            secondMovie = self.getRandomMovie()
+
+        content += "<h1>Tommorrow's Movie</h1>"
+        content += "<p>" + secondMovie + "</p>"
 
         self.response.write(content)
 
